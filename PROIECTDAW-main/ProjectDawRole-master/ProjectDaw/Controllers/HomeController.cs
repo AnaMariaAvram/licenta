389a2ae395252d1f5a350c57c41dc20d4e74aa33
AnaMariaAvram <ana_maria2602@yahoo.com>
2022-09-06 22:46:59 +0300

    d

---
+++ PROIECTDAW-main/ProjectDawRole-master/ProjectDaw/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using ProjectDaw.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectDaw.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            if (Request.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Product");
+            }
+
+            var products = (from product in db.Products
+                            select product);
+
+            ViewBag.FirstProduct = products.First();
+            ViewBag.Products = products.OrderBy(o => o.Price).Skip(1).Take(2);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ PROIECTDAW-main/ProjectDawRole-master/ProjectDaw/Controllers/ProductController.cs
@@ -0,0 +1,316 @@
+﻿using Microsoft.AspNet.Identity;
+using ProjectDaw.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectDaw.Controllers
+{
+    [Authorize]
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();        // GET: Product
+        private int _perPage = 3;
+
+        //GET
+        [Authorize(Roles = "User,Colaborator,Administrator")]
+        public ActionResult Index(string sortBy, string sort, string searching)
+        {
+            var products = db.Products.Include("Category").Include("User");
+
+            if (TempData.ContainsKey("message"))
+            {
+                ViewBag.message = TempData["message"].ToString();
+            }
+            ViewBag.esteAdmin = User.IsInRole("Administrator");
+
+            //search
+            ViewBag.Products = products.Where(x => (x.Title.Contains(searching) || x.Description.Contains(searching) || x.Review.Contains(searching)) || searching == null).ToList();
+            
+            //sort ascending/descending
+            switch(sortBy)
+            {
+                case "Price":
+                    switch(sort)
+                    {
+                        case "1":
+                            ViewBag.Products = products.OrderBy(x => x.Price);
+                            break;
+                        case "2":
+                            ViewBag.Products = products.OrderByDescending(x => x.Price);
+                            break;
+                        default:
+                            ViewBag.Products = products.Where(x => (x.Title.Contains(searching) || x.Description.Contains(searching) || x.Review.Contains(searching)) || searching == null).ToList();
+                            break;
+                    }
+                    break;
+                case "Rating":
+                    switch (sort)
+                    {
+                        case "1":
+                            ViewBag.Products = products.OrderBy(x => x.Rating);
+                            break;
+                        case "2":
+                            ViewBag.Products = products.OrderByDescending(x => x.Rating);
+                            break;
+                        default:
+                            ViewBag.Products = products.Where(x => (x.Title.Contains(searching) || x.Description.Contains(searching) || x.Review.Contains(searching)) || searching == null).ToList();
+                            break;
+                    }
+                    break;
+                default:
+                    ViewBag.Products = products.Where(x => (x.Title.Contains(searching) || x.Description.Contains(searching) || x.Review.Contains(searching)) || searching == null).ToList();
+                    break;
+            }
+      
+            return View();
+
+            
+        }
+
+
+       [Authorize(Roles = "User,Colaborator,Administrator")]
+
+        public ActionResult Show(int id)
+        {
+            Product product = db.Products.Find(id);
+            ViewBag.afisareButoane = false;
+            if (User.IsInRole("Colaborator") || User.IsInRole("Administrator"))
+            {
+                ViewBag.afisareButoane = true;
+            }
+
+            ViewBag.esteAdmin = User.IsInRole("Administrator");
+            ViewBag.utilizatorCurent = User.Identity.GetUserId();
+
+            return View(product);
+
+        }
+        [Authorize(Roles = "Colaborator,Administrator")]
+        public ActionResult New()
+        {
+            Product product = new Product();
+
+            product.Categories = GetAllCategories();
+            //Preluam Id-ul utilizatorului curent
+            product.UserId = User.Identity.GetUserId();
+
+            return View(product);
+        }
+
+  
+
+
+        [HttpPost]
+        [Authorize(Roles = "Colaborator,Administrator")]
+        [ValidateInput(false)]
+
+        public ActionResult New(Product product)
+        {
+            product.Categories = GetAllCategories();
+            product.UserId = User.Identity.GetUserId();
+            if (User.IsInRole("Administrator"))
+            {
+                product.Approved = true;
+            }
+
+            HttpPostedFileBase postedFile = Request.Files["ImageFile"];
+
+            if (postedFile == null)
+            {
+                product.ImagePath = null;
+                return View(product);
+            }
+            if (User.IsInRole("Colaborator"))
+            {
+                product.Approved = false;
+            }
+
+            //TO DO
+            // Change image name so 2 users can add the same photo
+            product.ImagePath = postedFile.FileName;
+            var imagePath = HttpContext.Server.MapPath("~/Images/") + postedFile.FileName;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    postedFile.SaveAs(imagePath);
+                    db.Products.Add(product);
+                    db.SaveChanges();
+                    TempData["message"] = "Produsul a fost adaugat!";
+
+                    return RedirectToAction("Index");
+                } 
+                else
+                {
+                    return View(product);
+                }
+            }
+            catch (Exception e)
+            {
+                return View(product);
+            }
+        }
+        [Authorize(Roles = "Colaborator,Administrator")]
+        public ActionResult Edit(int id)
+        {
+            Product product = db.Products.Find(id);
+            ViewBag.Product = product;
+            product.Categories = GetAllCategories();
+
+       
+            if (product.UserId == User.Identity.GetUserId() || User.IsInRole("Administrator"))
+            {
+                return View(product);
+            }
+            else
+            {
+                TempData["message"] = "Nu aveti dreptul sa faceti modificari asupra unui articol care nu va apartine!";
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPut]
+        [Authorize(Roles = "Colaborator,Administrator")]
+        public ActionResult Edit(int id, Product requestProduct)
+        {
+            requestProduct.Categories = GetAllCategories();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Product product = db.Products.Find(id);
+                    if (product.UserId == User.Identity.GetUserId() ||
+                        User.IsInRole("Administrator"))
+                    {
+                        if (TryUpdateModel(product))
+                        {
+                            product.Title = requestProduct.Title;
+                            product.Description = requestProduct.Description;
+                            product.Price = requestProduct.Price;
+                            product.Rating = requestProduct.Rating;
+                            product.Review = requestProduct.Review;
+                            product.CategoryId = requestProduct.CategoryId;
+                            HttpPostedFileBase postedFile = Request.Files["ImageFile"];
+
+                            if (postedFile == null)
+                            {
+                                //product.ImagePath = null;
+                                //return View(product);
+                            }
+
+                            //TO DO
+                            // Change image name so 2 users can add the same photo
+                            product.ImagePath = postedFile.FileName;
+                            var imagePath = HttpContext.Server.MapPath("~/Images/") + postedFile.FileName;
+
+                            postedFile.SaveAs(imagePath);
+                            
+                            db.SaveChanges();
+                            TempData["message"] = "Produsul a fost modificat!";
+                        }
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        TempData["message"] = "Nu aveti dreptul sa faceti modificari asupra unui articol care nu va apartine!";
+                        return RedirectToAction("Index");
+                    }
+
+                }
+                else
+                {
+                    return View(requestProduct);
+                }
+
+            }
+            catch (Exception e)
+            {
+                return View(requestProduct);
+            }
+        }
+        [NonAction]
+        public IEnumerable<SelectListItem> GetAllCategories()
+        {
+            // generam o lista goala
+            var selectList = new List<SelectListItem>();
+            // Extragem toate categoriile din baza de date
+            var categories = from cat in db.Categories
+                             select cat;
+            // iteram prin categorii
+            foreach (var category in categories)
+            {
+                // Adaugam in lista elementele necesare pentru dropdown
+                selectList.Add(new SelectListItem
+                {
+                    Value = category.CategoryId.ToString(),
+                    Text = category.CategoryName.ToString()
+                });
+            }
+            // returnam lista de categorii
+            return selectList;
+        }
+        [HttpDelete]
+        [Authorize(Roles = "Colaborator,Administrator")]
+        public ActionResult Delete(int id)
+        {
+            Product product = db.Products.Find(id);
+            if (product.UserId == User.Identity.GetUserId() || User.IsInRole("Administrator"))
+            {
+                db.Products.Remove(product);
+                db.SaveChanges();
+                TempData["message"] = "Articolul a fost sters!";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["message"] = "Nu aveti dreptul sa stergeti un articol care nu va apartine!";
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Administrator")]
+        public ActionResult Approved(int id)
+        {
+            Product product = db.Products.Find(id);
+            if (User.IsInRole("Administrator"))
+            {
+                product.Approved = true;
+                db.SaveChanges();
+                TempData["message"] = "Articolul a fost aprobat!";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["message"] = "Nu aveti dreptul sa stergeti un articol care nu va apartine!";
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpDelete]
+        [Authorize(Roles = "Administrator")]
+        public ActionResult Cancel(int id)
+        {
+            Product product = db.Products.Find(id);
+            if (User.IsInRole("Administrator"))
+            {
+                db.Products.Remove(product);
+                db.SaveChanges();
+                TempData["message"] = "Produsul a fost respins!";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["message"] = "Nu aveti dreptul sa stergeti un articol care nu va apartine!";
+                return RedirectToAction("Index");
+            }
+        }
+
+
+
+    }
+}
